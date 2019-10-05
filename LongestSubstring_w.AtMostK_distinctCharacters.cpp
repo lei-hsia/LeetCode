b51135a58f3157a3b4a12b1f12f87739a90a5085
LEI XIA <lx2196@columbia.edu>
2019-10-05 15:28:49 -0400

    Create LongestSubstring_w.AtMostK_distinctCharacters.cpp

---
+++ LongestSubstring_w.AtMostK_distinctCharacters.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int lengthOfLongestSubstringKDistinct(string s, int k) {
+        int res = 0, left = 0;
+        unordered_map<char, int> m;
+        for (int i = 0; i < s.size(); ++i) {
+            ++m[s[i]];
+            while (m.size() > k) {
+                if (--m[s[left]] == 0) m.erase(s[left]);
+                ++left;
+            }
+            res = max(res, i - left + 1);
+        }
+        return res;
+    }
+};
