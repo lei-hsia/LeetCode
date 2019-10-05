75dcb1429e97ecc5416c785b5e9d5db89a14efc4
LEI XIA <lx2196@columbia.edu>
2019-10-05 15:29:22 -0400

    Create LongestSubstring_w.AtMost2_distinctCharacters.cpp

---
+++ LongestSubstring_w.AtMost2_distinctCharacters.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int lengthOfLongestSubstringTwoDistinct(string s) {
+        int res = 0, left = 0;
+        unordered_map<char, int> m;
+        for (int i = 0; i < s.size(); ++i) {
+            ++m[s[i]];
+            while (m.size() > 2) {
+                if (--m[s[left]] == 0) m.erase(s[left]);
+                ++left;
+            }
+            res = max(res, i - left + 1);
+        }
+        return res;
+    }
+};
