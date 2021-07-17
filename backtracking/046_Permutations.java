95596929d3a52d426dbf8247993c8f72b2948db0
realLei <leileetcodekaggle@163.com>
2021-07-17 15:34:43 +0800

    Add files via upload

---
+++ backtracking/046_Permutations.java
@@ -0,0 +1,27 @@
+```java
+// 046
+class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> res = new ArrayList();
+        List<Integer> tmp = new ArrayList();
+        permute(nums, tmp, res);
+        return res;
+    }
+    
+    public void permute(int[] nums, List<Integer> tmp, List<List<Integer>> res) {
+        if (nums.length == tmp.size()) {
+            res.add(new ArrayList(tmp));
+            return;
+        }
+        
+        int n = nums.length;
+        for (int i = 0; i < n; ++i) {
+            if (tmp.contains(nums[i])) continue;
+            tmp.add(nums[i]);
+            permute(nums, tmp, res);
+            tmp.remove(tmp.size() - 1);
+        }
+    }
+}
+```
+
