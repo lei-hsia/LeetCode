d9c1d3fe30cc498b4d263f2c3b3ee5c4e12bb714
xialei07 <xialei07@meituan.com>
2021-09-11 17:19:55 +0800

    feature: add L235

---
+++ 2021-byNumber/src/main/java/L235V1.java
@@ -0,0 +1,15 @@
+/**
+ * @author lei
+ * @date 09/11/2021 5:19 PM
+ *
+ * version: didn't enhance characteristics of a BST. Just a BT
+ */
+public class L235V1 {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null || root == p || root == q) return root;
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        if (left != null  && right != null) return root;
+        return left != null ? left : right;
+    }
+}
